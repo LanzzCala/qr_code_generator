c745622094e24d9d029845b938a0e4e24c5b35db
Lanzz Cala <lanzzcala@gmail.com>
2025-04-08 21:02:59 -0700

    Begin function to collect text and convert it to qrcode

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,4 +1,14 @@
 #Install the needed libraries
+import qrcode
 #Create a function that collects text and converts to a qrcode
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version= 1,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
 #Save the qrcode as an image
 #Run the function
