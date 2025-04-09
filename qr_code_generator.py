63fa104a5c5033f1b8d0264c58db4ad965c82377
Lanzz Cala <lanzzcala@gmail.com>
2025-04-08 21:13:08 -0700

    Have the qrcode be saved into image. Run test of url

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,14 +1,20 @@
 #Install the needed libraries
 import qrcode
+import qrcode.constants
 #Create a function that collects text and converts to a qrcode
 def generate_qrcode(text):
     qr = qrcode.QRCode(
         version= 1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4
     )
+
     qr.add_data(text)
     qr.make(fit=True)
-
-#Save the qrcode as an image
+    #Save the qrcode as an image
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
 #Run the function
+
+generate_qrcode("https://pypi.org/project/qrcode/")
